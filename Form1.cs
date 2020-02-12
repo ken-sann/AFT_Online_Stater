7f7086d535c2989aef8f26331b0117c1576dc06a
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2020-02-13 01:13:32 +0800

    合并代码

--- Form1.cs
+++ Form1.cs
@@ -1,55 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace AFT_Online_Stater
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {/*
-            var process = new Process
-            {
-                StartInfo =
-                {
-                    FileName = "cmd.exe",
-                    Arguments = "ipconfig/flushdns",
-                    WindowStyle = ProcessWindowStyle.Hidden,
-                    CreateNoWindow = true,
-                }
-            };
-            process.Start();
-            process.WaitForExit();
-            process.Close();*/
-            //创建启动对象 
-            ProcessStartInfo startInfo = new ProcessStartInfo
-            {
-                //设置运行文件 
-                FileName = "inject.exe",
-                //设置启动动作,确保以管理员身份运行 
-                Verb = "runas",
-                Arguments = $"-d -k divahook.dll diva.exe"
-            };
-            //如果不是管理员，则启动UAC 
-            Process.Start(startInfo); 
-            Close();
-        }
-    }
-}
-=======
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -92,4 +41,3 @@ namespace AFT_Online_Stater
         }
     }
 }
->>>>>>> 23f92b2769af8b38f1b16b0c352cec72aebacc8e
