2477ba83809c9adc202fb8aa410c0bb18f032c58
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2020-02-20 18:21:04 +0800

    增加新建桌面图标功能

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Windows.Forms;
+using IWshRuntimeLibrary;
 
 namespace AFT_Online_Stater
 {
@@ -9,6 +11,22 @@ namespace AFT_Online_Stater
         public Form1()
         {
             InitializeComponent();
+        }
+        public static void CreateShortcut(string directory, string shortcutName, string targetPath,
+            string description = null, string iconLocation = null)
+        {
+            string shortcutPath = Path.Combine(directory, string.Format("{0}.lnk", shortcutName));
+            if (!System.IO.File.Exists(shortcutPath))
+            {
+                WshShell shell = new WshShell();
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);//创建快捷方式对象
+                shortcut.TargetPath = targetPath;//指定目标路径
+                shortcut.WorkingDirectory = Path.GetDirectoryName(targetPath);//设置起始位置
+                shortcut.WindowStyle = 1;//设置运行方式，默认为常规窗口
+                shortcut.Description = description;//设置备注
+                shortcut.IconLocation = string.IsNullOrWhiteSpace(iconLocation) ? targetPath : iconLocation;//设置图标路径
+                shortcut.Save();//保存快捷方式
+            }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -33,7 +51,15 @@ namespace AFT_Online_Stater
                     divaProcess.Kill();//结束进程
                 }
             }
-
+            try
+            {
+                CreateShortcut(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "AFT联机启动器", Process.GetCurrentProcess().MainModule.FileName);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("创建桌面图标失败");
+                throw;
+            }
             Process.Start(startInfo);//启动diva.exe 
             Close();
         }
